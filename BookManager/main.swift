05ed0a781e23c0fd9d2333f4a5f0d1683038fd84
Hyung jin Jun <junhyungjin@me.com>
2016-04-11 23:44:19 +0900

    Initial Commit

---
+++ BookManager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BookManager
+//
+//  Created by junhyungjin on 2016. 4. 11..
+//  Copyright © 2016년 Hyung Jun Jun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
