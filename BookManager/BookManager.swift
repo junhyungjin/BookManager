34d486776809d4abd069ab6450f7e15584138f40
Hyung jin Jun <junhyungjin@me.com>
2016-04-12 23:12:32 +0900

    First Commit

---
+++ BookManager/BookManager.swift
@@ -0,0 +1,33 @@
+//
+//  BookManager.swift
+//  BookManager
+//
+//  Created by junhyungjin on 2016. 4. 11..
+//  Copyright © 2016년 Hyung Jun Jun. All rights reserved.
+//
+
+import Foundation
+
+class BookManager {
+    var bookList = [Book]()
+    
+    func addBook(BookObject: Book) {
+        
+    }
+    
+//    func showAllBook() -> String {
+//    
+//    }
+//    
+//    func countBook() -> Int {
+//        
+//    }
+//    
+//    func findBook(name: String) -> String? {
+//        
+//    }
+//    
+//    func removeBook(name: String) {
+//        
+//    }
+}
--- BookManager/main.swift
+++ BookManager/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+var book1 = Book()
+book1.name = "암호의 해석"
+book1.genre = "Science"
+book1.author = "루돌프 키펜한"
 
+book1.bookPrint()
+
+var book2 = Book()
+book2.name = "알고리즘 문제해결"
+book2.genre = "Computer"
+book2.author = "구종만"
+
+book2.bookPrint()
+
+var book3 = Book()
+book3.name = "죄와 벌"
+book3.genre = "고전"
+book3.author = "톨스토이"
+
+book3.bookPrint()
